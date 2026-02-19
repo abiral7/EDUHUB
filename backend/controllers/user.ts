d500a28d5bdca0a592f61da19e04c833cbd753d7
Abiral Gurung <abiralg7@gmail.com>
2026-02-19 21:09:27 +0545

    Created user controller to register the user

---
+++ backend/controllers/user.ts
@@ -0,0 +1,45 @@
+import type { Request, Response } from "express";
+import User from "../models/user";
+
+export const registerUser = async (req: Request, res: Response):Promise<void> => {
+    try {
+        const {
+            name, email, password, role, studentClass, teacherSubject, isActive
+        } = req.body;
+
+        // check if user exists
+        const existingUser = await User.findOne({email});
+
+        if(existingUser){
+            res.status(400).json({message:"User already exists"});
+            return;
+        }
+
+        //Create user
+        const newUser = await User.create({
+            name,
+            email,
+            password,
+            role,
+            studentClass,
+            teacherSubject,
+            isActive
+        })
+
+        if(newUser){
+            res.status(201).json({
+                _id: newUser.id,
+                name: newUser.name,
+                email: newUser.email,
+                role: newUser.email,
+                studentClass: newUser.studentClass,
+                teacherSubject: newUser.teacherSubject,
+                message: "User registered successfully"
+            })
+        }else{
+            res.status(400).json({message:"Invalid user data"})
+        }
+    } catch (error) {
+        res.status(500).json({message: "Server Error", error});
+    }
+}
