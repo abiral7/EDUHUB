47988e684102a66546b7e2a9fffd8cf061166346
Abiral Gurung <abiralg7@gmail.com>
2026-02-19 20:52:18 +0545

    added User model with password hasing/matching

---
+++ backend/models/user.ts
@@ -0,0 +1,50 @@
+import mongoose, {Document, Schema} from "mongoose";
+import bcrypt from "bcryptjs";
+
+export enum UserRole{
+    ADMIN = "admin",
+    STUDENT = "student",
+    PARENT = "parent",
+    TEACHER = "teacher"
+}
+
+export type userRoles = "admin" | "student" | "parent" | "teacher";
+
+export interface IUser extends Document {
+    name: string;
+    email: string;
+    password: string;
+    role: userRoles;
+    isActive: boolean;
+    studentClass?: string | null;
+    teacherSubject?: string[] | null;
+    matchPassword: (enteredPassword: string) => Promise<boolean>;
+}
+
+const userSchema: Schema<IUser> = new Schema({
+    name: {type: String, required:true},
+    email: {type: String, required:true},
+    password: {type:String, required:true},
+    role: {type:String, enum:Object.values(UserRole), required:true, default: UserRole.STUDENT},
+    isActive: {type:Boolean, required:true},
+    studentClass: {type:mongoose.Schema.Types.ObjectId, ref:'Class'},
+    teacherSubject: [{type:mongoose.Schema.Types.ObjectId, ref:'Subject'}],
+    
+},{
+    timestamps:true,
+});
+
+// per-save middleware to  hash password
+userSchema.pre<IUser>("save", async function (){
+    if(!this.isModified("password")) return
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+});
+
+//method to match entered password with hash password
+userSchema.methods.matchPassword = async function (enteredPassword:string) {
+    return await bcrypt.compare(enteredPassword, this.password);
+};
+
+const User = mongoose.model<IUser>("User", userSchema);
+export default User;
