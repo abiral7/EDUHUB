e86baffce95e2c54bc4a601851aa3c94329f98cb
Abiral Gurung <abiralg7@gmail.com>
2026-02-24 01:05:17 +0545

    User CRUD API

--- backend/controllers/user.ts
+++ backend/controllers/user.ts
@@ -2,6 +2,7 @@ import type { Request, Response } from "express";
 import User from "../models/user";
 import { generateToken } from "../utils/generateToken";
 import { logActivity } from "../utils/activityLog";
+import type { AuthRequrest } from "../middleware/auth";
 
 // @desc    Register a new user
 // @route   POST /api/users/register
@@ -72,5 +73,122 @@ export const loginUser = async (req: Request, res:Response):Promise<void> => {
          }
     } catch (error) {
         res.status(500).json({message:"Server error",error});
+    }
+}
+
+// @desc    Update user
+// @route   PATCH /api/users/update
+// @access   Private Admin Only
+export const updateUser = async (req: Request, res:Response):Promise<void> => {
+    try {
+        const user = await User.findById(req.params.id);
+        if(user){
+            user.name = req.body.name || user.name;
+            user.email = req.body.email || user.email;
+            user.isActive = req.body.isActive !== undefined ? req.body.isActive : user.isActive;
+            user.studentClass = req.body.studentClass || user.studentClass;
+            user.teacherSubject = req.body.teacherSubject || user.teacherSubject;
+            if(req.body.password){
+                user.password = req.body.password
+            }
+            const updatedUser = await user.save();
+
+            res.status(201).json({
+                _id: updatedUser.id,
+                name: updatedUser.name,
+                email: updatedUser.email,
+                role: updatedUser.email,
+                isActive: updatedUser.isActive,
+                studentClass: updatedUser.studentClass,
+                teacherSubject: updatedUser.teacherSubject,
+                message: "User updated successfully"
+            })
+
+            if((req as any).user){
+                await logActivity({
+                    userId: (req as any).user._id,
+                    action: "Update User",
+                    details: `Updated user with email ${updatedUser.email}`
+                })
+            }
+        }else {
+            res.status(404).json({message:"User not found"})
+        }
+    } catch (error) {
+        res.status(500).json({message:"Server Error", error})
+    }
+}
+
+
+// @desc    delete user
+// @route   DELETE /api/users/delete
+// @access   Private Admin Only
+export const deleteUser = async (req: Request, res:Response):Promise<void> => {
+    try {
+        const user = await User.findById(req.params.id)
+        if(user){
+            await user.deleteOne()
+            if((req as any).user){
+                await logActivity({
+                    userId: (req as any).user._id,
+                    action: "Update User",
+                    details: `Deleted user with email ${user.email}`
+                })
+            }
+            res.json({message:"User deleted successfully"})
+        }else{
+            res.status(404).json({message:"User not found"})
+        }
+    } catch (error) {
+        res.status(500).json({message:"Server Error", error})
+    }
+}
+
+// @desc    get user
+// @route   GET /api/users/all
+// @access   Private Admin Only
+export const getAllUser = async (req: Request, res:Response):Promise<void> =>{
+    try {
+        const page = Number(req.query.page) || 1;
+        const limit = Number(req.query.limit) || 10;
+        const skip = (page -1) * limit;
+
+        const count = await User.countDocuments()
+
+        const users = await User.find()
+        .sort({createdAt: -1})// latest first
+        .skip(skip)
+        .limit(limit)
+
+        res.json({
+            users,
+            page,
+            pages: Math.ceil(count / limit),
+            total: count
+        })
+    } catch (error) {
+        res.status(500).json({message:"Server Error", error})
+    }
+}
+
+// @desc    get user
+// @route   GET /api/users/all
+// @access   Private Admin Only
+export const getUserProfile = async (req: AuthRequrest, res: Response):Promise<void> =>{
+    try {
+        if(req.user){
+            res.json({
+                user: {
+                    _id: req.user._id,
+                    name: req.user.name,
+                    email: req.user.email,
+                    role: req.user.email
+                }
+            })
+        }else{
+            res.status(404).json({message:"User not found"});
+        }
+    } catch (error) {
+        res.status(500).json({message:"Server error", error});
     }
 }
--- backend/routes/user.ts
+++ backend/routes/user.ts
@@ -1,12 +1,14 @@
 import express from 'express'
-import { loginUser, register } from '../controllers/user'
+import { deleteUser, getAllUser, loginUser, register, updateUser } from '../controllers/user'
 import { authorize, protect } from '../middleware/auth';
 
 const userRoutes = express.Router()
 
 //make sure to use protect to get the user token
 userRoutes.post("/register", protect, authorize(["admin", "teacher"]), register);
-
 userRoutes.post("/login", loginUser);
+userRoutes.delete("/delete/:id", protect, authorize(["admin", "teacher"]), deleteUser);
+userRoutes.patch("/update/:id", protect, authorize(["admin", "teacher"]), updateUser);
+userRoutes.get("/all", protect, authorize(["admin", "teacher"]), getAllUser);
 
 export default userRoutes;
