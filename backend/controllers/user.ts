6d4477229ec0f2046507234ebdf3e54ff4d14910
Abiral Gurung <abiralg7@gmail.com>
2026-02-24 20:21:47 +0545

    Logged user out and all user fetched with search filter and pagination

--- backend/controllers/user.ts
+++ backend/controllers/user.ts
@@ -151,20 +151,41 @@ export const getAllUser = async (req: Request, res:Response):Promise<void> =>{
     try {
         const page = Number(req.query.page) || 1;
         const limit = Number(req.query.limit) || 10;
+        const role = req.query.role as String;
+        const search = req.query.search as String;
         const skip = (page -1) * limit;
 
-        const count = await User.countDocuments()
+        //Builder filter object
+        const filter: any = {}
 
-        const users = await User.find()
-        .sort({createdAt: -1})// latest first
-        .skip(skip)
-        .limit(limit)
+        if(role && role !== "all" && role !== ""){
+            filter.role = role;
+        }
 
+        if(search){
+            filter.$or = [
+                {name: {$regex: search, $options:"i"}},
+                {email: {$regex: search, $options:"i"}}
+            ];
+        }
+        const [total, users] = await Promise.all([
+            User.countDocuments(filter),
+            User.find(filter)
+            .select("-password")
+            // .populate("studentClass","_id name section")
+            // .populate("teacherSubject","_id name code")
+            .sort({createdAt: -1})
+            .skip(skip)
+            .limit(limit)
+        ])
         res.json({
             users,
-            page,
-            pages: Math.ceil(count / limit),
-            total: count
+            pagination:{
+                total,
+                page,
+                pages: Math.ceil(total/limit),
+                limit
+            }
         })
     } catch (error) {
         res.status(500).json({message:"Server Error", error})
@@ -172,7 +193,7 @@ export const getAllUser = async (req: Request, res:Response):Promise<void> =>{
 }
 
 // @desc    get user
-// @route   GET /api/users/all
+// @route   GET /api/users/profile
 // @access   Private Admin Only
 export const getUserProfile = async (req: AuthRequrest, res: Response):Promise<void> =>{
     try {
@@ -186,9 +207,23 @@ export const getUserProfile = async (req: AuthRequrest, res: Response):Promise<v
                 }
             })
         }else{
-            res.status(404).json({message:"User not found"});
+            res.status(401).json({message:"Not authorized"});
         }
     } catch (error) {
         res.status(500).json({message:"Server error", error});
+    }
+}
+// @desc    logout user user
+// @route   GET /api/users/logout
+// @access   Private Admin Only
+export const logout = async (req: AuthRequrest, res: Response):Promise<void> =>{
+    try {
+        res.cookie("jwt","",{
+            httpOnly: true,
+            expires: new Date(0)
+        });
+        res.json({message:"User logged out successfully"});
+    } catch (error) {
+        res.status(500).json({message:"Server Error", error})
     }
 }
--- backend/routes/user.ts
+++ backend/routes/user.ts
@@ -1,5 +1,5 @@
 import express from 'express'
-import { deleteUser, getAllUser, loginUser, register, updateUser } from '../controllers/user'
+import { deleteUser, getAllUser, getUserProfile, loginUser, logout, register, updateUser } from '../controllers/user'
 import { authorize, protect } from '../middleware/auth';
 
 const userRoutes = express.Router()
@@ -7,6 +7,8 @@ const userRoutes = express.Router()
 //make sure to use protect to get the user token
 userRoutes.post("/register", protect, authorize(["admin", "teacher"]), register);
 userRoutes.post("/login", loginUser);
+userRoutes.post("/logout", logout);
+userRoutes.get("/profile", protect, authorize(["admin", "teacher"]), getUserProfile);
 userRoutes.delete("/delete/:id", protect, authorize(["admin", "teacher"]), deleteUser);
 userRoutes.patch("/update/:id", protect, authorize(["admin", "teacher"]), updateUser);
 userRoutes.get("/all", protect, authorize(["admin", "teacher"]), getAllUser);
